2abb7f37c4f4c21d66f84af3010322154183d5cf
Dennis Duan <gwduan@gmail.com>
2024-10-18 08:57:57 +0800

    Add some test data (all user's password is 'password')

---
+++ scripts/test-data.sql
@@ -0,0 +1,12 @@
+USE echo_demo;
+
+INSERT INTO users(name, password, age, reg_date) VALUES('Jolanda', '$argon2id$v=19$m=65536,t=1,p=12$QAH4t9WRRT3qGkBQzbB4hg$wA7mP4+3s7jlcaRyc3tUYiLl4bg8SKfUecMaNQq/1EI', 31, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Jenny',   '$argon2id$v=19$m=65536,t=1,p=12$qX2XGmctYvA/GspZky+K1Q$spmREBL92Umw9fE7vFRagt4sFsVzcBcnXjgiCy3O+U8', 30, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Haley',   '$argon2id$v=19$m=65536,t=1,p=12$tx0ymzW4DLE4aPsZiQGzYg$YOHaqqaSVvtizbcKH+dykT0LNlCJW+ZSvQV/n2s0U9s', 26, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Evie',    '$argon2id$v=19$m=65536,t=1,p=12$kyHgM7a4fkrwZLPkuRRaPw$u8xXbu6DjlYx/pBQFppr4Eaa4jSUSpPiVx5FEnuZMgU', 27, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Kate',    '$argon2id$v=19$m=65536,t=1,p=12$zgmHnv1oqJX7X6IeNiArOQ$tZ3ZT/xisnlM5Wsv3+by6MERbYMg72n3SkpaNLXbAqw', 28, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Rebecca', '$argon2id$v=19$m=65536,t=1,p=12$5wCW+6r6MeSmjPVgcbOPrw$GlsOLUM48CH6hnde/ZusKe8tKzmciF5f+WUpx9La86k', 33, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Emily',   '$argon2id$v=19$m=65536,t=1,p=12$2LWMAwdwnk7zhGe3r/BLAg$VcjUwZovkV5ifmmYdowC6Teijf9ITrhYIqo5/rtiri8', 36, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Pauline', '$argon2id$v=19$m=65536,t=1,p=12$fI/CWxc+mEbHsIZu/21a6A$2geIxpgmaKDYKxubLso9GHeOtOtHmYnjhP+seXcmhXU', 32, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Candice', '$argon2id$v=19$m=65536,t=1,p=12$Jy6pN7vgmP/pY3l9wkxrxQ$0EGEDZmbx/IokKCrXjVgUZqsiECddOO2o2vL5W9YYTA', 32, now());
+INSERT INTO users(name, password, age, reg_date) VALUES('Loana',   '$argon2id$v=19$m=65536,t=1,p=12$sc7jYsTuRFI82JgKC2niQw$dIKzWdAgpuvLrz/BLmHg2qGZPknc6lo9lYmCwh30UoI', 25, now());
